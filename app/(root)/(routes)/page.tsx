ca03f5e236d22831acc262404632c0b34198ad20
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-08-26 23:37:15 -0300

    feat: create to search component with tecnical input

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import SearchInput from "@/components/Search-Input";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
     </div>
   )
 }
