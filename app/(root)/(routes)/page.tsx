7649ee70b1247c9d9e6ec2b4d8408fb6c857bff8
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-09-04 23:52:02 -0300

    feat: init component in use client button category

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,14 @@
+import Categories from "@/components/Categories";
 import SearchInput from "@/components/Search-Input";
+import prismadb from "@/lib/prismadb";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
